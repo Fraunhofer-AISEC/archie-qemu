18372371972878d289e07e9130d6d91408cab9d3
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2020-08-16 18:20:30 +0200

    added readout of registers to test and subscribe to every memory access cb

--- tests/plugin/develop/empty.c
+++ tests/plugin/develop/empty.c
@@ -22,21 +22,37 @@
 //#include "cpu.h"
 //#include "exec/exec-all.h"
 
+int flag = 0;
+
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
 static void tb_exec_cb(unsigned int vcpu_index, void *userdata)
 {
 	g_autoptr(GString) out = g_string_new("");
 	g_string_append(out, "Executed\n");
+	/*Test if tb flush is successfull*/
 	plugin_flush_tb();
 	g_string_append(out, "Flushing tb cache\n");
+	/*Test if write can handle the endianess of the arm processor (it does) -> do not care for endianess, qemu does it*/
+	g_string_append_printf(out,"REG0 value: %08x\n", read_arm_reg(0));
+	write_arm_reg(0, read_arm_reg(0));
+	g_string_append_printf(out,"REG0 value: %08x\n", read_arm_reg(0));
+
 	qemu_plugin_outs(out->str);
 }
 
+static void vcpu_memaccess(unsigned int vcpu_index, qemu_plugin_meminfo_t info, uint64_t vaddr, void *userdata)
+{
+	g_autoptr(GString) out = g_string_new("");
+	g_string_append_printf(out, "\n VCPU Index: %i\n vaddr: %8lx\n", vcpu_index, vaddr);
+
+	qemu_plugin_outs(out->str);
+}
+
+
 /*
- * Empty TB translation callback.
- * This allows us to measure the overhead of injecting and then
- * removing empty instrumentation.
+ * TB calback. It is currently used to test if it is possible to change the instructions that qemu translates
+ * Spoiler: it works. However manipulation must happen at n-1
  */
 static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
 {
@@ -48,8 +64,8 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
 	for(int i = 0; i < tb->n; i++)
 	{
         	struct qemu_plugin_insn *insn = qemu_plugin_tb_get_insn(tb, i);
-//		g_string_append_printf(out, "This is the HW address      %lx\n",    *((long unsigned int *)(insn->haddr)));
 		g_string_append_printf(out, "%8lx ", insn->vaddr);
+		/*If a certain instruction is reached, manipulate the register file. Also make sure to flush the tb after execution. Result, the change is going into effect at the next translation*/
 		if (insn->vaddr == 0x80000dc)
 		{
 			g_string_append_printf(out, "%s\n", qemu_plugin_insn_disas( insn));
@@ -57,21 +73,40 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
 			CPUState *cpu = current_cpu;
 			char tmp[2];
 			int ret;
+			/*Read value, add 1 and then write it back*/
 			ret = cpu_memory_rw_debug(cpu, 0x80000dc, tmp, 2, 0);
 			if( ret == -1)
 			{
 				return;
 			}
-			tmp[1]++;
+			tmp[0]++;
 			cpu_memory_rw_debug(cpu, 0x80000dc, tmp, 2, 1);
 //			*(((char *)tb->haddr1  ) + i*2) = (*((char *)tb->haddr1 + i*2) + 1);	
-//			int cpu_memory_rw_debug(CPUState *cpu, target_ulong addr, void *ptr, target_ulong len, bool is_write)
 		}
 		g_string_append_printf(out, "%s\n", qemu_plugin_insn_disas( insn));
+
+		qemu_plugin_register_vcpu_mem_cb(insn, vcpu_memaccess, QEMU_PLUGIN_CB_RW_REGS, QEMU_PLUGIN_MEM_RW,NULL);
 	}
 	g_string_append_printf(out, "Virt2 value %8lx, hw addr2 %p\n", tb->vaddr2, tb->haddr2);
-	qemu_plugin_outs(out->str);
 
+	if (flag == 0)
+	{
+		flag = 1;
+		CPUState *cpu = current_cpu;
+                char tmp[2];
+                int ret;
+                /*Read value, add 1 and then write it back*/
+                ret = cpu_memory_rw_debug(cpu, 0x80000dc, tmp, 2, 0);
+                if( ret == -1)
+                {
+                        return;
+                }
+                tmp[0]++;
+                cpu_memory_rw_debug(cpu, 0x80000dc, tmp, 2, 1);
+		g_string_append(out,"This is the first tb ever and instructions are changed");
+
+	}
+	qemu_plugin_outs(out->str);
 
 }
 
@@ -80,6 +115,21 @@ static void vcpu_init_cb(qemu_plugin_id_t id, unsigned int vcpu_index)
 	g_autoptr(GString) out = g_string_new("");
 	g_string_printf(out, "CPU Initialised\n");
 	qemu_plugin_outs(out->str);
+	/*Read value, add 1 and then write it back*/
+/*	CPUState *cpu = current_cpu;
+	char tmp[2];
+	int ret;
+	g_string_printf(out, "try to read from memory location\n");
+	qemu_plugin_outs(out->str);
+	ret = cpu_memory_rw_debug(cpu, 0x80000dc, tmp, 2, 0);
+	if( ret == -1)
+	{
+		return;
+	}
+	tmp[0]++;
+	g_string_printf(out,"try to write to memory location\n");
+	qemu_plugin_outs(out->str);
+	cpu_memory_rw_debug(cpu, 0x80000dc, tmp, 2, 1);*/
 }
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
@@ -99,6 +149,6 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
     qemu_plugin_outs(out->str);
     qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
     qemu_plugin_register_vcpu_init_cb( id,  vcpu_init_cb);
-
+    flag = 0;
     return 0;
 }
