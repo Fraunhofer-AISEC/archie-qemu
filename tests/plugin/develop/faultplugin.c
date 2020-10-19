930de059b91523bf0a8e903b3f16494b1ca9e057
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2020-10-19 18:10:29 +0200

    Removed hashtable. Replaced it with avl tree. inserted callbacks to automatically fill avl tree with elements if not already existing. Also added tb exec event hanlde to increment counter and add element to linked list to find out, in which order the tbs where executed

--- tests/plugin/develop/faultplugin.c
+++ tests/plugin/develop/faultplugin.c
@@ -18,6 +18,7 @@
 #include <qemu/plugin.h>
 #include "hw/core/cpu.h"
 
+#include "lib/avl.h"
 
 //DEBUG
 #include <errno.h>
@@ -80,134 +81,56 @@ int	exec_callback;
 int 	first_tb;
 
 
-/* Hashtable for saving tb informations*/
+/*Output TB data structures*/
 typedef struct tb_info_t tb_info_t;
-typedef struct
+typedef struct tb_info_t
 {
-	uint64_t base_address;
-	uint64_t size;
-	uint64_t instruction_count;
-	GString * assembler;
-	uint64_t num_of_exec; // Number of executions(aka a counter)
-	tb_info_t *next;
+        uint64_t base_address;
+        uint64_t size;
+        uint64_t instruction_count;
+        GString * assembler;
+        uint64_t num_of_exec; // Number of executions(aka a counter)
+        tb_info_t *next;
 }tb_info_t;
 
-typedef struct hash_tb_info_t hash_tb_info_t;
-typedef struct
+tb_info_t *tb_info_list; 
+
+typedef struct tb_exec_order_t tb_exec_order_t;
+typedef struct tb_exec_order_t
 {
-	int valid;
-	int key; //base address
-	tb_info_t *tb;
-	hash_tb_info_t* next;
-}hash_tb_info_t;
+	tb_info_t *tb_info;
+	tb_exec_order_t *prev;
+}tb_exec_order_t;
 
-/* Global hashtable variables */
-#define FIRST_HASH_TABLE_SIZE 256
-/* Hash table array */
-hash_tb_info_t hash_table[];
-/* Hash table size */
-int hash_table_size;
+tb_exec_order_t *tb_exec_order_list;
 
-hash_tb_info_t* dummy_hash;
 
-hash_tb_info_t first_hash_tb_info;
-hash_tb_info_t last_hash_tb_info;
+/*AVL global variables*/
+struct avl_table *tb_avl_root;
 
-void fill_hash_table()
+/*Needed for avl library. it will determen which element is bigger*/
+int tb_comparison_func(const void *tbl_a, const void *tbl_b, void * tbl_param)
 {
-	// this function initilieses the hash tabel array with invalid hash entries.
-	for(int i = 0; i < hash_table_size; i++)
+	//g_autoptr(GString) out = g_string_new("");
+	const tb_info_t * tb_a = tbl_a;
+	const tb_info_t * tb_b = tbl_b;
+	//g_string_printf(out, "[Info]: Compare function called\n");
+	//g_string_append_printf(out, "[Info]: a baseaddress: %p, b baseaddress: %p\n",tbl_a, tbl_b);
+	//qemu_plugin_outs(out->str);
+	if(tb_a->base_address < tb_b->base_address)
 	{
-		hash_table[i] = dummy_hash;
+
+		return -1;
 	}
+	else if(tb_a->base_address > tb_b->base_address) return 1;
+	else return 0;
 }
 
-void create_hash_table()
-{
-	hash_table = malloc(sizeof(hash_tb_info_t) * hash_table_size);
-	fill_hash_table();
-}
+//void tbl_item_func(void *tbl_item, void *tbl_param)
+//{
+//}
 
-void delete_hash_table()
-{
-	free(hash_table);
-}
-
-uint64_t get_key_hash(uint64_t key)
-{
-	return key % hash_table_size;
-}
-
-void init_hash_table()
-{
-	/* Initialise first the init size */
-	hash_table_size = FIRST_HASH_TABLE_SIZE;
-	first_hash_tb_info = NULL;
-	last_hash_tb_info = NULL;
-	/* Initialise dummy item*/
-	dummy_hash = malloc(sizeof(hash_tb_info_t));
-	dummy_hash->valid = 0;
-	dummy_hash->key = -1;
-	dummy_hash->tb = NULL;
-	/* Initialise array */
-	create_hash_table();
-	/*hash table can now be filled*/
-}
-
-int increase_hash_table()
-{
-	hash_tb_info_t* current = first_hash_tb_info;
-
-	delete_hash_table();
-
-	//increase hash table size
-	hash_table_size = hash_table_size * 2;
-
-	//reinit memory
-	create_hash_table();
-	while(current != NULL)
-	{
-		uint64_t hkey = get_key_hash( current->key);
-		if(hash_table[hkey]->valid == 1 )
-		{
-			return -1;
-		}
-		hash_table[hkey] = current;
-		current = current->next;
-	}	
-	return 0;
-}
-
-void insert_element(tb_info_t* tb)
-{
-	uint64_t hkey = 0;
-	int valid = 0;
-	hash_tb_info_t* element = malloc(sizeof(hash_tb_info_t));
-	element->tb = tb;
-	element->key = tb->base_address;
-	element->valid = 0;
-	element->next = NULL;
-	hkey = get_key_hash(element->key);
-	/* is it empty?*/
-	valid = hash_table[hkey]->valid;
-	while(valid == 1)
-	{
-		/* Hash collition. Need to increase hashtable*/
-		if(increase_hash_table() == 0)//everything went fine in the increase
-		{
-			hkey = get_key_hash(element->key);
-			valid = hash_table[hkey]->valid;
-		}
-		/* if not true, valid will not be 1*/
-	}
-	/* now have a valid hkey*/
-	/* Insert element into htable*/
-	hash_table[hkey] = element;
-	/* Insert into linked list for increase purposes*/
-	element->next = first_hash_tb_info;
-	first_hash_tb_info = element;
-}
-
+//void * tbl_copy_func(void *tbl_item, void *tbl_param);
 /*QEMU plugin Version control*/
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
@@ -231,17 +154,17 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
 int parse_args(int argc, char **argv, GString *out)
 {
-	g_string_append_printf(out, "Starting argparsing\n");
+	g_string_append_printf(out, "[Info]: Starting argparsing\n");
 	if(argc != 3)
 	{
-		g_string_append_printf(out, "Not the right ammount of arguments! %i\n", argc);
+		g_string_append_printf(out, "[ERROR]: Not the right ammount of arguments! %i\n", argc);
 		return -1;
 	}
-	g_string_append_printf(out, "Start readout of control fifo %s\n", *(argv+0));
+	g_string_append_printf(out, "[Info]: Start readout of control fifo %s\n", *(argv+0));
 	pipes->control = open(*(argv+0), FIFO_READ);
-	g_string_append_printf(out, "Start readout of config fifo %s\n", *(argv+1));
+	g_string_append_printf(out, "[Info]: Start readout of config fifo %s\n", *(argv+1));
 	pipes->config = open(*(argv+1), FIFO_READ);
-	g_string_append_printf(out, "Start readout of data fifo %s\n", *(argv+2));
+	g_string_append_printf(out, "[Info]:Start readout of data fifo %s\n", *(argv+2));
 	pipes->data = open(*(argv+2), FIFO_WRITE);
 	return 0;
 }
@@ -252,7 +175,7 @@ uint64_t char_to_uint64(char *c, int size_c)
 	g_autoptr(GString) out = g_string_new("");
 	uint64_t tmp = 0;
 	int i = 0;
-	g_string_printf(out, "This is the conversion function: ");
+	g_string_printf(out, "[Info]: This is the conversion function: ");
 	for(i = 0; i < size_c; i++)
 	{
 		g_string_append_printf(out, " 0x%x",(char) *(c + i));
@@ -323,21 +246,21 @@ int qemu_setup_config()
 	uint8_t buf[16];
 	uint64_t target_len = 8;
 	uint64_t tmp = 0xffffffffffffffff;
-	g_string_printf(out, "Start redout of FIFO\n");
+	g_string_printf(out, "[Info]: Start redout of FIFO\n");
 	for(int i = 0; i < 7; i++)
 	{
-		g_string_append_printf(out, "Parameter %i\n", i);
+		g_string_append_printf(out, "[Info]: Parameter %i\n", i);
 		//qemu_plugin_outs(out->str);
 		ssize_t readout = 0;
 		ssize_t read_bytes = 0;
 		while(target_len > read_bytes)
 		{
 			readout = read(pipes->config, buf + readout, target_len - readout );
-			g_string_append_printf(out, "DEBUG: readout %li, target_len %li \n", readout, target_len);
+			g_string_append_printf(out, "[DEBUG]: readout %li, target_len %li \n", readout, target_len);
 			if(readout == -1)
 			{
-				g_string_append_printf(out, "DEBUG: Value is negativ, Somthing happend in read: %s\n", strerror(errno));
-				g_string_append_printf(out, "DEBUG: File descriptor is : %i\n", pipes->config);
+				g_string_append_printf(out, "[DEBUG]: Value is negativ, Somthing happend in read: %s\n", strerror(errno));
+				g_string_append_printf(out, "[DEBUG]: File descriptor is : %i\n", pipes->config);
 				//qemu_plugin_outs(out->str);
 				//g_string_printf(out, "  \n");
 			}
@@ -347,33 +270,33 @@ int qemu_setup_config()
 				readout = 0;
 			}
 		}
-		g_string_append_printf(out, "done readout of pipe\n");
+		g_string_append_printf(out, "[Info]: done readout of pipe\n");
 		//qemu_plugin_outs(out->str);
 		switch(i)
 		{
 			case 0:
 				fault_address = char_to_uint64(buf, 8);
 				target_len = 8;
-				g_string_append_printf(out, "fault address: 0x%lx\n", fault_address);
+				g_string_append_printf(out, "[Info]: fault address: 0x%lx\n", fault_address);
 				break;
 			case 1:
 				fault_type = char_to_uint64(buf, 8);
 				target_len = 8;
-				g_string_append_printf(out, "fault address: 0x%lx\n", fault_type);
+				g_string_append_printf(out, "[Info]: fault address: 0x%lx\n", fault_type);
 				break;
 			case 2:
 				fault_model = char_to_uint64(buf, target_len);
 				target_len = 8;
-				g_string_append_printf(out, "fault address: 0x%lx\n", fault_model);
+				g_string_append_printf(out, "[Info]: fault address: 0x%lx\n", fault_model);
 				break;
 			case 3:
 				fault_lifetime = char_to_uint64(buf, target_len);
 				target_len = 16;
-				g_string_append_printf(out, "fault address: 0x%lx\n", fault_lifetime);
+				g_string_append_printf(out, "[Info]: fault address: 0x%lx\n", fault_lifetime);
 				break;
 			case 4:
 				//fault_mask = buf[15] << 15*8 | buf[14] << 14*8 | buf[13] << 13*8 | buf[12] << 12*8 | buf[11] << 11*8 | buf[10] << 10*8 | buf[9] << 9*8 | buf[8] << 8*8 | buf[7] << 7*8 | buf[6] << 6*8 | buf[5] << 5*8 | buf[4] << 4*8 |buf[3] << 3*8 | buf[2] << 2*8 | buf[1] << 1*8 | buf[0] << 0*8;
-				g_string_append(out, "fault mask: ");
+				g_string_append(out, "[Info]: fault mask: ");
 				for(int j = 0; j < 16; j++)
 				{
 					fault_mask[j] = buf[j];
@@ -385,16 +308,16 @@ int qemu_setup_config()
 			case 5:
 				fault_trigger_address = char_to_uint64(buf, target_len);
 				target_len = 8;
-				g_string_append_printf(out, "fault address: 0x%lx\n", fault_trigger_address);
+				g_string_append_printf(out, "[Info]: fault address: 0x%lx\n", fault_trigger_address);
 				break;
 			case 6:
 				fault_trigger_hitcounter = char_to_uint64(buf, target_len);
 				target_len = 8;
-				g_string_append_printf(out, "fault address: 0x%lx\n", fault_trigger_hitcounter);
+				g_string_append_printf(out, "[Info]: fault address: 0x%lx\n", fault_trigger_hitcounter);
 				break;
 		}
 	}
-	g_string_append(out, "Fault pipe read done\n");
+	g_string_append(out, "[Info]: Fault pipe read done\n");
 	qemu_plugin_outs(out->str);
 	return add_fault(fault_address, fault_type, fault_model, fault_lifetime, fault_mask, fault_trigger_address, fault_trigger_hitcounter);
 }
@@ -512,7 +435,7 @@ fault_list_t * get_fault_struct_by_exec(uint64_t exec_callback_address)
 int register_fault_trigger_addresses()
 {
 	g_autoptr(GString) out = g_string_new("");
-	g_string_printf(out, "Calculate number of faults\n");
+	g_string_printf(out, "[Info]: Calculate number of faults .......");
 	/*Select first element of list*/
 	fault_list_t * current = first_fault;
 	int i = 0;
@@ -522,34 +445,34 @@ int register_fault_trigger_addresses()
 		i++;
 		current = return_next(current);
 	}
-	g_string_append_printf(out, "number_of_faults %i\n",i);
+	g_string_append_printf(out, "%i\n",i);
 	if(i == 0)
 	{
-		g_string_append(out, "No fault found!\n");
+		g_string_append(out, "[ERROR]:No fault found!\n");
 		qemu_plugin_outs(out->str);
 		return -1;
 	}
 	/* Reset back to firs element*/
 	current = first_fault;
 	fault_number = i;
-	g_string_append_printf(out, "Fault number %i\n", fault_number);
+	g_string_append_printf(out, "[DEBUG]: Fault number %i\n", fault_number);
 	/* Reserve Memory vor "Vector"*/
 	fault_trigger_addresses = malloc(sizeof(uint64_t) * fault_number);
 	fault_addresses = malloc(sizeof(uint64_t) * fault_number);
 	if(fault_trigger_addresses == NULL || fault_addresses == NULL)
 	{
-		g_string_append_printf(out, "malloc failed here in registerfaulttrigger\n");
+		g_string_append_printf(out, "[ERROR]: malloc failed here in registerfaulttrigger\n");
 		qemu_plugin_outs(out->str);
 		return -1;
 	}
-	g_string_append(out, "Start registering faults\n");
+	g_string_append(out, "[Info]: Start registering faults\n");
 	for(int j = 0; j < i; j++)
 	{
 		/* Fill Vector with value*/
 		*(fault_trigger_addresses + j) = get_fault_trigger_address(current);
 		*(fault_addresses + j) = 0;	
-		g_string_append_printf(out, "fault trigger addresses: %p\n", fault_trigger_addresses+j);
-		g_string_append_printf(out, "fault addresses: %p\n", fault_addresses+j);
+		g_string_append_printf(out, "[Fault]: fault trigger addresses: %p\n", fault_trigger_addresses+j);
+		g_string_append_printf(out, "[Fault]: fault addresses: %p\n", fault_addresses+j);
 		current = return_next(current);	
 	}
 	qemu_plugin_outs(out->str);
@@ -788,7 +711,7 @@ size_t calculate_bytesize_instructions(struct qemu_plugin_tb *tb)
 	struct qemu_plugin_insn * insn_first = qemu_plugin_tb_get_insn(tb, 0);
 	struct qemu_plugin_insn * insn_last = qemu_plugin_tb_get_insn(tb, tb->n -1);
 	uint64_t size = (insn_last->vaddr - insn_first->vaddr) + insn_last->data->len;
-	g_string_printf(out, "tb instruction size is %li \n", size);
+	g_string_printf(out, "[CALC]: tb instruction size is %li \n", size);
 
 	qemu_plugin_outs(out->str);
 	return (size_t) size;
@@ -900,6 +823,25 @@ void eval_exec_callback(struct qemu_plugin_tb *tb, int exec_callback_number)
 }
 
 
+void tb_exec_data_event(unsigned int vcpu_index, void *vcurrent)
+{
+	tb_info_t *tb_info = vcurrent;
+	tb_info->num_of_exec++;
+	tb_exec_order_t *last = malloc(sizeof(tb_exec_order_t));
+	last->tb_info = tb_info;
+	last->prev = tb_exec_order_list;
+	tb_exec_order_list = last;
+	//TODO
+	//Build Abbortion logic here. Because this will be executed every tb
+	//
+	//
+//DEBUG	
+//	g_autoptr(GString) out = g_string_new("");
+//	g_string_printf(out, "[TB_exec]: ID: %x, Execs %i\n", tb_info->base_address, tb_info->num_of_exec);
+//	qemu_plugin_outs(out->str);
+}
+
+
 /**
  * handle_tb_translate_event
  *
@@ -938,12 +880,60 @@ void handle_tb_translate_event(struct qemu_plugin_tb *tb)
 
 void handle_tb_translate_data(struct qemu_plugin_tb *tb)
 {
+	g_autoptr(GString) out = g_string_new("");
+	g_string_printf(out, "\n");
 	//TODO
 	//virt1 ist id der tb
-	g_autoptr(GString) out = g_string_new("");
+	tb_info_t tmp;
+	tmp.base_address = tb->vaddr;
+	g_string_append_printf(out, "[TB Info]: Search TB......");
+	//qemu_plugin_outs(out->str);
+	tb_info_t *tb_information = (tb_info_t *) avl_find(tb_avl_root, &tmp); 	
+	if(tb_information == NULL)
+	{
+		tb_information = malloc(sizeof(tb_info_t));
+		if(tb_information == NULL)
+		{
+			return;
+		}
+		tb_information->base_address = tb->vaddr;
+		tb_information->instruction_count = tb->n;
+		tb_information->assembler = decode_assembler(tb);
+		tb_information->num_of_exec = 0;
+		tb_information->size = calculate_bytesize_instructions(tb);
+		tb_information->next = tb_info_list;
+		tb_info_list = tb_information;
+		g_string_append(out, "Not Found\n");
+		if( avl_insert(tb_avl_root, tb_information) != NULL)
+		{
+			qemu_plugin_outs("[ERROR]: Somthing went wrong in avl instert");
+			return;
+		}
+		else
+		{
+			if(avl_find(tb_avl_root, &tmp) != tb_information)
+			{
+				qemu_plugin_outs("[ERROR]: Conntent changed!");
+				return;
+			}
+		}
+		g_string_append(out, "[TB Info]: Done insertion into avl\n");
+		//qemu_plugin_outs(out->str);
+	}
+	else
+	{
+		g_string_append(out, "Found\n");
+	}
+
+	qemu_plugin_register_vcpu_tb_exec_cb(tb, tb_exec_data_event, QEMU_PLUGIN_CB_RW_REGS, tb_information);
+	
+
+	//DEBUG
 	GString *assembler = decode_assembler(tb);
-	g_string_printf(out, "[TB Info] tb id: %8lx\n[TB Info] tb size: %i\n[TB Info] Assembler:\n%s", tb->vaddr, tb->n, assembler->str);
+	g_string_append_printf(out, "[TB Info] tb id: %8lx\n[TB Info] tb size: %i\n[TB Info] Assembler:\n%s", tb->vaddr, tb->n, assembler->str);
 	g_string_free(assembler, TRUE);
+
+
 	qemu_plugin_outs(out->str);
 	
 }
@@ -999,6 +989,9 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
 	fault_number = 0;
 	fault_trigger_addresses = NULL;
 	fault_addresses = NULL;
+	tb_avl_root = NULL;
+	tb_info_list = NULL;
+	tb_exec_order_list = NULL;
 	exec_callback = 0;
 	first_tb = 0;
 	g_autoptr(GString) out = g_string_new("");
@@ -1006,13 +999,13 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
 	g_string_append_printf(out, "Current Version of QEMU Plugin is %i, Min Version is %i\n", info->version.cur, info->version.min);
 	if(strcmp(info->target_name, "arm") < 0)
 	{
-		g_string_append(out, "Arbort plugin, as this architecture is currently not supported!\n");
+		g_string_append(out, "[ERROR]: Arbort plugin, as this architecture is currently not supported!\n");
 		qemu_plugin_outs(out->str);
 		return -1;
 	}
 	pipes = malloc(sizeof(fifos_t));
 	if(pipes == NULL)
-	{
+	{	g_string_append(out, "[ERROR]: Pipe struct not malloced\n");
 		goto ABBORT;
 	}
 	pipes->control = -1;
@@ -1021,38 +1014,45 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
 	/*Start Argparsing and open up the Fifos*/
 	if(parse_args(argc, argv, out) != 0)
 	{
-		g_string_append_printf(out, "Initialisation of FIFO failed!\n");
+		g_string_append_printf(out, "[ERROR]: Initialisation of FIFO failed!\n");
 		qemu_plugin_outs(out->str);
 		return -1;
 	}
-	g_string_append_printf(out, "Initialisation of FIFO Succeded!\n");
-	g_string_append_printf(out, "Readout config FIFO\n");
+	g_string_append_printf(out, "[Info]: Initialisation of FIFO.......Done!\n");
+	g_string_append_printf(out, "[Info]: Readout config FIFO\n");
 	qemu_plugin_outs(out->str);
 	g_string_printf(out, " ");
 	if( qemu_setup_config() < 0)
 	{
 		goto ABBORT;
 	}
-	g_string_append_printf(out, "Linked list entry address: %p\n", first_fault);	
+	g_string_append_printf(out, "[Info]: Linked list entry address: %p\n", first_fault);	
 
-	g_string_append_printf(out, "Register fault trigger addresses\n");
+	g_string_append_printf(out, "[Info]: Register fault trigger addresses\n");
 	qemu_plugin_outs(out->str);
 	g_string_printf(out, " ");
 	if(register_fault_trigger_addresses() < 0 )
 	{
 		goto ABBORT;
 	}
-	g_string_append_printf(out, "Number of triggers: %i\n", fault_number);
-	g_string_append(out, "Register VCPU tb trans callback\n");
+	g_string_append_printf(out, "[Info]: Number of triggers: %i\n", fault_number);
+	g_string_append(out, "[Info]: Register VCPU tb trans callback\n");
 	qemu_plugin_register_vcpu_tb_trans_cb( id, vcpu_translateblock_translation_event);
-
-	g_string_append_printf(out, "Reached end of Initialisation, starting guest now\n");
+	g_string_append(out, "[Info]: Initialise TB avl tree ....");
+	tb_avl_root = avl_create( &tb_comparison_func, NULL, NULL);
+	if(tb_avl_root == NULL)
+	{
+		g_string_append(out, "ERROR\n[ERROR] TB avl tree initialisation failed\n");
+		goto ABBORT;
+	}
+	g_string_append(out, "Done\n");
+	g_string_append_printf(out, "[Start]: Reached end of Initialisation, starting guest now\n");
 	qemu_plugin_outs(out->str);
 	return 0;
 ABBORT:
 	delete_fault_trigger_addresses();
 	delete_fault_queue();
-	g_string_append(out, "Somthing went wrong. Abborting now!\n");
+	g_string_append(out, "[ERROR]: Somthing went wrong. Abborting now!\n");
 	qemu_plugin_outs(out->str);
 	return -1;
 }
