c07ef95f759fb56a0f90daab201438b78d68bac2
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2021-02-01 18:49:28 +0100

    Moved tb_exec data collection from faultplugin.c to tb_exec_data_collection.c This should make it a little bit more easier to understand this part of the program

--- tests/plugin/develop/faultplugin.c
+++ tests/plugin/develop/faultplugin.c
@@ -25,6 +25,7 @@
 #include "fault_list.h"
 #include "fault_injection.h"
 #include "tb_info_data_collection.h"
+#include "tb_exec_data_collection.h"
 //DEBUG
 #include <errno.h>
 #include <string.h>
@@ -72,34 +73,8 @@ fault_trigger_t end_point;
 int tb_info_enabled;
 
 
-typedef struct tb_exec_order_t tb_exec_order_t;
-typedef struct tb_exec_order_t
-{
-	tb_info_t *tb_info;
-	tb_exec_order_t *prev;
-	tb_exec_order_t *next;
-}tb_exec_order_t;
-
-tb_exec_order_t *tb_exec_order_list;
-uint64_t num_exec_order;
 int tb_exec_order_enabled;
 
-/**
- * tb_exec_order_free()
- *
- * free linked list of tb_exec_order_t elements. It does not free the tb_info_t inside.
- * These must be freed seperatly with tb_info_free()
- */
-void tb_exec_order_free()
-{
-	tb_exec_order_t *item;
-	while(tb_exec_order_list != NULL)
-	{
-		item = tb_exec_order_list;
-		tb_exec_order_list = tb_exec_order_list->prev;
-		free(item);
-	}
-}
 
 
 
@@ -691,42 +666,6 @@ int plugin_write_to_data_pipe(char *str, size_t len)
 
 
 
-/**
- * plugin_dump_tb_exec_order
- *
- * Print the order of translation blocks executed. Also provide a counter number, that it can be later resorted in python
- */
-void plugin_dump_tb_exec_order()
-{
-	uint64_t i = 0;
-	if(tb_exec_order_list == NULL)
-	{
-		return;
-	}
-	g_autoptr(GString) out = g_string_new("");
-	g_string_printf(out, "$$$[TB Exec]:\n");
-	plugin_write_to_data_pipe(out->str, out->len);
-	tb_exec_order_t *item =  tb_exec_order_list;
-
-	while(item->prev != NULL)
-	{
-		i++;
-		item = item->prev;
-	}
-	i++;
-	if(i != num_exec_order)
-	{
-		qemu_plugin_outs("[WARNING]: i und numexec sind nicht gleich !\n");
-	}
-	i = 0;
-	while(item != NULL)
-	{
-		g_string_printf(out, "$$ 0x%lx | %li \n", item->tb_info->base_address, i);
-		plugin_write_to_data_pipe(out->str, out->len);
-		item = item->next;
-		i++;
-	}
-}
 
 /**
  * plugin_dump_mem_information
@@ -809,37 +748,6 @@ void plugin_end_information_dump()
 	//
 }
 
-/**
- * tb_exec_data_event
- * 
- * Function to collect the exec data about translation blocks
- *
- * vcpu_index: current index of cpu the callback was triggered from
- * vcurrent: pointer to tb_info struct of the current tb
- */
-void tb_exec_data_event(unsigned int vcpu_index, void *vcurrent)
-{
-	tb_info_t *tb_info = vcurrent;
-	if(tb_info != NULL)
-	{
-		tb_info->num_of_exec++;
-	}
-	tb_exec_order_t *last = malloc(sizeof(tb_exec_order_t));
-	last->tb_info = tb_info;
-	last->next = NULL;
-	last->prev = tb_exec_order_list;
-	if(tb_exec_order_list != NULL)
-	{
-		tb_exec_order_list->next = last;
-	}
-	tb_exec_order_list = last;
-	num_exec_order++;
-	//
-	//DEBUG	
-	//	g_autoptr(GString) out = g_string_new("");
-	//	g_string_printf(out, "[TB_exec]: ID: %x, Execs %i\n", tb_info->base_address, tb_info->num_of_exec);
-	//	qemu_plugin_outs(out->str);
-}
 
 void tb_exec_end_max_event(unsigned int vcpu_index, void *vcurrent)
 {
@@ -1230,10 +1138,7 @@ int initialise_plugin(GString * out, int argc, char **argv)
 	// It contains the pointer to fault structs, which livetime is not zero
 	// If livetime of fault reaches zero it undoes the fault. If zero it is permanent.
 	live_faults = NULL;
-	// List of execution order of tbs.
-	tb_exec_order_list = NULL;
-	// 
-	num_exec_order = 0;
+	tb_exec_order_init();
 	//
 	mem_info_list = NULL;
 	//
--- tests/plugin/develop/faultplugin.h
+++ tests/plugin/develop/faultplugin.h
@@ -15,5 +15,6 @@ int register_live_faults_callback(fault_list_t *fault);
 void invalidate_fault_trigger_address(int fault_trigger_number);
 
 int plugin_write_to_data_pipe(char *str, size_t len);
+ 
 
 #endif
